3cb9498338cf440d8b0831b0d34fb8aec32cfe8b
myrade-bf <0xaliasrowyan@gmail.com>
2018-04-04 16:12:27 -0400

    User match email and password

--- routes/api/users.js
+++ routes/api/users.js
@@ -46,4 +46,29 @@ router.post('/register', (req, res) => {
   });
 });
 
+// @route   GET api/users/login
+// @desc    Login User / Returning JWT Token
+// @access  Public
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find user by email
+  User.findOne({ email }).then(user => {
+    // Check for user
+    if (!user) {
+      return res.status(404).json({ email: 'User not found' });
+    }
+
+    // Check Password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        res.json({ msg: 'Success' });
+      } else {
+        return res.status(400).json({ password: 'Password incorrect' });
+      }
+    });
+  });
+});
+
 module.exports = router;

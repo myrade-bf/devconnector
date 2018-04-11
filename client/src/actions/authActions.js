25a679200c52d7fc1365814beda0690828012c34
myrade-bf <0xaliasrowyan@gmail.com>
2018-04-11 12:50:50 -0400

    Logout and navbar links

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -48,3 +48,13 @@ export const setCurrentUser = decoded => {
     payload: decoded
   };
 };
+
+// Log user out
+export const logoutUser = () => dispatch => {
+  // Remove token from localStorage
+  localStorage.removeItem('jwtToken');
+  // Remove auth header for future requests
+  setAuthToken(false);
+  // Set current user to {} which will set isAuthenticated to false
+  dispatch(setCurrentUser({}));
+};

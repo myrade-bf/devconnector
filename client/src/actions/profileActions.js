a991e13c5046bdc539d87f275f6cb0a8cd8fdaf3
myrade-bf <0xaliasrowyan@gmail.com>
2018-04-13 13:00:34 -0400

    Dashboard experience display and delete

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -66,6 +66,24 @@ export const addEducation = (eduData, history) => dispatch => {
     );
 };
 
+// Delete Experience
+export const deleteExperience = id => dispatch => {
+  axios
+    .delete(`/api/profile/experience/${id}`)
+    .then(res =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
 // Delete account & profile
 export const deleteAccount = () => dispatch => {
   if (window.confirm('Are you sure? This can NOT be undone!')) {

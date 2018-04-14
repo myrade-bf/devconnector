0c0523abed4018d53f5380e23e84ee6ae5e33cec
myrade-bf <0xaliasrowyan@gmail.com>
2018-04-14 09:22:39 -0400

    Add comment component and action

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -109,6 +109,24 @@ export const removeLike = id => dispatch => {
     );
 };
 
+// Add Comment
+export const addComment = (postId, commentData) => dispatch => {
+  axios
+    .post(`/api/posts/comment/${postId}`, commentData)
+    .then(res =>
+      dispatch({
+        type: GET_POST,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
 // Set loading state
 export const setPostLoading = () => {
   return {
--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import PostItem from '../posts/PostItem';
+import CommentForm from './CommentForm';
 import Spinner from '../common/Spinner';
 import { getPost } from '../../actions/postActions';
 
@@ -21,6 +22,7 @@ class Post extends Component {
       postContent = (
         <div>
           <PostItem post={post} showActions={false} />
+          <CommentForm postId={post._id} />
         </div>
       );
     }
